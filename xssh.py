c4dcd6de67090f12fa1d9e7fdd20e7a8255377ea
Iahiax <108660221+Iahiax@users.noreply.github.com>
2025-04-10 07:31:51 +0300

    Update xssh.py

--- xssh.py
+++ xssh.py
@@ -1,1 +1,11 @@
+import paramiko
 
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(hostname="ssh.pythonanywhere.com", username="xyahyax", password="Yahia-1411")
+
+stdin, stdout, stderr = ssh.exec_command("ls")
+print(stdout.read().decode())
+
+ssh.close()
